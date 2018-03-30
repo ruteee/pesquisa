939381cefe961373840c7ad185cd413425f3c533
ruteee <rute.s.aberu@gmail.com>
2018-03-29 23:42:43 -0300

    Limpeza do notebook de TE e ciação do arquivo py com TE

---
+++ transferEntropy.py
@@ -0,0 +1,206 @@
+import pandas as pd
+import numpy as np
+
+import datetime as dt
+import random
+import time
+import math
+import itertools
+
+import statsmodels.api as sm
+
+from IPython.display import clear_output
+
+def surrogate(a):
+    a_diff = np.diff(a)
+    begin = np.where(a_diff > 0)[0]
+    end = np.where(a_diff < 0)[0]
+    
+    if begin.size > end.size:
+        end = np.append(end, a.size)
+    elif begin.size < end.size:
+        begin = np.insert(begin, 0, 0) 
+    elif begin.size == 0 and end.size == 0:
+        return a.copy()
+    elif np.all(begin > end):
+        begin = np.insert(begin, 0, 0)
+        end = np.append(end, a.size)
+    
+    n_seq = np.max([begin.size, end.size])
+    a_surr = np.zeros(a.shape)
+    p_seq = np.random.randint(0, a.size - max(end - begin), size=n_seq)
+    for i in np.random.permutation(n_seq):
+        len_seq = end[i] - begin[i]
+        a_surr[p_seq[i]:p_seq[i] + len_seq] = a[begin[i]:end[i]]
+    return a_surr
+
+def get_lim_index(cdf, lim):
+    summation = 0
+    index = 0
+    for i in np.arange(0, cdf.size):
+        if cdf[i] > lim:
+            index = i
+            break
+    return index
+
+def significance_test(k,l,h,sup_lim, n, a,b):
+    '''
+        significance_test(a,b,k,l,h,sup_lim, n)
+    '''
+    transferEntropies = []    
+    
+    np.random.seed(int(time.time()))
+    for i in np.arange(0,n):
+        surrogate_a = surrogate(a.copy())
+        transferEntropies.append(te(k,l,h,surrogate_a[:],b, 'serie_a', 'serie_b'))
+        
+    kde = sm.nonparametric.KDEUnivariate(transferEntropies)
+    kde.fit()
+    
+    
+    lvl_sig = kde.icdf[get_lim_index(kde.cdf, sup_lim)]
+    return lvl_sig
+
+
+def joint_probability_new(k,l,h, a, b, lbl_a, lbl_b):
+    '''
+        k B time horizon
+        l A time horizon
+        h instant in the future of serie B
+        a, b array type'''
+    
+    numStates=2**(k+l+1)
+    combinations = list(map(list, itertools.product([0, 1], repeat=k+l+1)))
+    prob_cnjt = np.zeros(numStates)
+    
+    #Alarm Series A (cause), B (effect), same len
+    #teste   
+
+    matrix_nova = np.matrix([b[1:],b[:-1],a[:-1]]).T
+    df = pd.DataFrame(matrix_nova, columns = ['b_ftr', lbl_b, lbl_a])
+    gpd = df.groupby(['b_ftr', lbl_b, lbl_a], as_index=False).size().reset_index(name='Count')
+    total = sum(gpd['Count'])
+    
+    for i in np.arange(0,gpd.shape[0]):
+        comb = [e for e in gpd.iloc[i][0:3].values.tolist()]
+        idx = combinations.index(comb)
+        prob_cnjt[idx] = gpd.iloc[i]['Count']/total
+
+    return prob_cnjt
+
+def joint_probability(k,l, h, a, b):
+    '''
+        k B time horizon
+        l A time horizon
+        h instant in the future of serie B
+        a, b array type'''
+
+    #Alarm Series A (cause), B (effect), same len
+    #tested
+    sizeSeries = a.size
+    transEntropy = 0
+    numStates = 2**(k + l  + 1)
+    combinations = list(map(list, itertools.product([0, 1], repeat=k+l+1)))
+    counting = np.zeros(numStates)
+    prob_cnjt = np.zeros(numStates)
+    a_prob_ind = []
+    b_prob_ind = []
+    #joitn probability p(i_sub_t+1), i_sub_t**k, j_sub_t**l)
+    inicio = np.max([k,l]) - 1
+    for i in np.arange(inicio, sizeSeries - h):
+        for hk in np.arange(0,k):
+                b_prob_ind.append(b[i - hk])
+        for hl in np.arange(0,l):
+                a_prob_ind.append(a[i - hl])
+
+        ab = [b[i + h]] + b_prob_ind + a_prob_ind 
+        index_comb = combinations.index(ab)
+        counting[index_comb] = counting[index_comb] + 1
+
+        a_prob_ind = []
+        b_prob_ind = []
+
+    total = sum(counting)
+  
+    prob_cnjt = counting/total
+     
+    return prob_cnjt
+
+
+def joint_prob_ih_ik(k,l, joint_prob_ih_ik_jl):
+	#Joint probability evaluation p(i_t+h, i_t**k)
+    states_ith_ik = list(map(list, itertools.product([0, 1], repeat=k + 1)))
+    combinations = list(map(list, itertools.product([0, 1], repeat=k+l+1))) 
+    p_jnt_ith_ik = np.zeros(2**(k+1))
+    
+    for i, state in enumerate(states_ith_ik):
+        for j, comb in enumerate(combinations):
+            if comb[0:k+1] == state:
+                p_jnt_ith_ik[i] = p_jnt_ith_ik[i] + joint_prob_ih_ik_jl[j]
+    return p_jnt_ith_ik
+
+def conditional_prob(k,l,joint_prob):
+    states = list(map(list, itertools.product([0, 1], repeat=k+l)))
+    combinations = list(map(list, itertools.product([0, 1], repeat=k+l+1)))
+
+    size = int(joint_prob.size/2)
+    conditional = np.zeros(2**(k+l+1))
+
+    for i,state in enumerate(states):
+        index_zero = combinations.index([0] + state)
+        prob_zero = joint_prob[index_zero]
+
+        index_one = combinations.index([1] + state)
+        prob_one = joint_prob[index_one]
+
+        if(prob_zero + prob_one != 0):
+            conditional[i] = prob_zero/(prob_zero+ prob_one)
+            conditional[i + 2**(k+l)] = prob_one/(prob_zero+ prob_one)
+    return conditional
+
+def conditional_div(k,l,conditional_num, conditional_den):
+    combinations = list(map(list, itertools.product([0, 1], repeat=k+l+1)))
+    conditional_division = np.zeros(conditional_num.size)
+    states_den = list(map(list, itertools.product([0, 1], repeat=1+k)))
+    for j, comb in enumerate(combinations):
+        if(conditional_den[states_den.index(comb[0:k+1])] != 0):
+            conditional_division[j] = conditional_num[j]/conditional_den[states_den.index(comb[0:k+1])]            
+    return conditional_division
+              
+
+def te(k,l,h,a,b, lbl_a, lbl_b):
+    '''
+        transentropy a->b
+        te(k,l,h,a,b)
+        k - dimension of b
+        l - dimension of aDataFrame count duplicate rows and ...
+        h -> instant in the future of a
+    '''
+    #joint_p_ih_ik_jl = joint_probability_new(k,l,h,a,b, lbl_a, lbl_b)
+    joint_p_ih_ik_jl = joint_probability(k,l,h,a,b)
+    
+    joint_p_ih_ik = joint_prob_ih_ik(k,l, joint_p_ih_ik_jl)
+    conditional_num = conditional_prob(k,l,joint_p_ih_ik_jl)
+    conditional_den = conditional_prob(k,0, joint_p_ih_ik)    
+    div = conditional_div(k,l,conditional_num, conditional_den)
+    
+    #log2 from the division of the conditionals -> #p(i_sub_t+h|i_sub_t**k, j_sub_t**l) /p(i_sub_t+h|i_t**k)
+    
+    log2_div_cond = np.log2(div[div!=0])
+    te = np.sum(joint_p_ih_ik_jl[div!=0]*log2_div_cond)
+    return te
+
+def transferEntropy_case(dist_df, pcnt_sig, k, l, h):
+    start = time.clock()
+    transEntropy = np.zeros([dist_df.columns.size,dist_df.columns.size])
+    sigValues =  np.zeros([dist_df.columns.size,dist_df.columns.size])
+    for i in np.arange(0, dist_df.columns.size):
+        for j in np.arange(0, dist_df.columns.size):
+            print('trans ', dist_df.columns[i], dist_df.columns[j])
+            if(j != i + dist_df.columns.size/2 and j!=i and j != i - dist_df.columns.size/2):
+                transEntropy[i][j] = te(k,l,h,dist_df[dist_df.columns[i]], dist_df[dist_df.columns[j]],'serie_a', 'serie_b')
+            clear_output()
+    end = time.clock()   
+    
+    print(end - start)
+    return transEntropy  
